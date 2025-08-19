4ec80f775764f053e399bb1b0394f6e55338f996
Daniel John Baynosa <danieljohnbyns@gmail.com>
2025-08-19 17:09:30 +0800

    feat: implement collapsible functionality in PanelCard component and adjust transition timing

--- src/components/PanelCard.jsx
+++ src/components/PanelCard.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 
-import { Card, Flex, Empty } from 'antd';
+import { Card, Flex, Empty, Button } from 'antd';
+
+import { MinusOutlined, CaretDownOutlined } from '@ant-design/icons';
 
 /**
  * @param {{
@@ -18,12 +20,37 @@ const PanelCard = ({
 	contentMaxHeight,
 	...props
 }) => {
+	const [collapsed, setColapsed] = React.useState(false);
+
 	return (
-		<Card title={title} size='small' style={{ flex: 1, display: 'flex', height: '100%' }} {...props}>
-			<Flex vertical gap={8} style={{
-				position: 'relative',
-				height: '100%'
-			}}>
+		<Card
+			title={title}
+			size='small'
+			className='panel-card'
+			extra={
+				<Button
+					type='secondary'
+					size='small'
+					icon={collapsed ? <CaretDownOutlined /> : <MinusOutlined />}
+					onClick={() => { setColapsed(!collapsed) }}
+				/>
+			}
+			{...props}
+			style={{
+				flex: 1,
+				display: 'flex',
+				height: '100%',
+				maxHeight: collapsed ? 32 : 1024,
+				overflowY: 'hidden',
+				...props.style
+			}}
+			onClick={(e) => {
+				console.log(e.target.classList);
+				if (e.target.classList.contains('ant-card-head') || e.target.classList.contains('ant-card-head-title'))
+					setColapsed(!collapsed);
+			}}
+		>
+			<Flex vertical gap={8} style={{ position: 'relative' }}>
 				<Flex vertical gap={8} style={{ flex: 1, minHeight: 128 }}>
 					{children || (
 						<Empty description='No content found' style={{ position: 'absolute', height: 128, top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }} />
