f60463a45dced1e6b9332939481bcca197b8c8e0
Daniel John Baynosa <danieljohnbyns@gmail.com>
2025-09-23 12:55:07 +0800

    feat: add transition duration to animation in Dashboard component

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -521,6 +521,7 @@ const Dashboard = () => {
 							initial={{ opacity: 0, x: 20 }}
 							animate={{ opacity: 1, x: 0 }}
 							exit={{ opacity: 0, x: -20 }}
+							transition={{ duration: 0.2 }}
 							style={{ width: '100%', minHeight: '100%' }}
 						>
 							<PagePropsProvider value={pageProps}>
